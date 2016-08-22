f18eec1f0335b4c7b6bfec2cf704095bd4eafda7
pvrfrx@gmail.com <javarush1>
2016-08-22 17:03:03 +0500

    add drawcharinposition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,22 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hi");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        reader.readLine();
+        DrawCharInPoint(1,2,'c');
+        DrawCharInPoint(3,2,'d');
+
+    }
+
+    public static void DrawCharInPoint(int x, int y, char c){
+        for (int i = 0; i < y-1; i++) {
+            System.out.println();
+        }
+        for (int i = 0; i < x-1; i++) {
+            System.out.print(' ');
+        }
+        System.out.print(c);
     }
 }
