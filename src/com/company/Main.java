64a0f756f13ecf66a446fc383fc1500254e3b4bb
pvrfrx@gmail.com <javarush1>
2016-08-22 16:08:10 +0500

    add readline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hi");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
     }
 }
